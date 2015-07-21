1f7a2d60a6434839f848f6baf8656fce45273c3b
Jesse Berman <jesse@clutchmobile.com>
2015-07-21 15:51:36 -0700

    Add documentation

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,8 @@
-# Creates a special matrix object that can cache its inverse.
+#' Creates a special matrix object that can cache its inverse.
+#'
+#' @param x An initial matrix to wrap.  The matrix must be invertible.
+#' @return The wrapped matrix.
+#' 
 makeCacheMatrix <- function(x = matrix()) {
     i <- NULL
     set <- function(y) {
@@ -13,8 +17,13 @@ makeCacheMatrix <- function(x = matrix()) {
          getinverse = getinverse)
 }
 
-# Computes the inverse of a matrix that was created
-# using the makeCacheMatrix function.
+#' Computes the inverse of a matrix that was created
+#' using the makeCacheMatrix function.
+#'
+#' @param x A matrix constructed via makeCacheMatrix.
+#' @param ... Additional arguments to pass to the solve function.
+#' @return The inverted matrix.
+#' 
 cacheSolve <- function(x, ...) {
     i <- x$getinverse()
     if(!is.null(i)) {
